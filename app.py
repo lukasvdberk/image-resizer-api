f74b3c5c20c405cf84a258510d428032ff35bad2
Lukas van den berk <lukasvdberk@gmail.com>
2020-06-07 22:24:43 +0200

    Added pillow resizing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import os
+from PIL import Image
+
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "uploads/"
@@ -8,6 +10,10 @@ app.config['UPLOAD_FOLDER'] = "uploads/"
 # https://pythonbasics.org/flask-upload-file/
 # TODO add max size in bytes
 # app.config['MAX_CONTENT_PATH'] = 8000
+
+def append_local_upload_dir(filename):
+  return os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(filename))
+
 
 @app.route("/")
 def index():
@@ -20,13 +26,16 @@ def resize():
     height = int(request.args.get('height'))
 
     f = request.files['image']
-    image_filename = f.save(
-      os.path.join(app.config['UPLOAD_FOLDER'], 
-      secure_filename(f.filename))
-    )
-    print(image_filename)
+
+    img_file_loc = append_local_upload_dir(f.filename) 
+    f.save(img_file_loc)
+
+    img_to_resize = Image.open(img_file_loc)
+    resized_img = img_to_resize.resize((width, height))
+    resized_img.save(img_file_loc, "PNG", optimize=True)
 
     return 'file uploaded successfully'
+    
   except ValueError:
     return "Make sure width and height are numbers"
   return "Hello World!"
